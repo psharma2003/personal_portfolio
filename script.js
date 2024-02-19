fdb255355be754f7b2a8852ef3c2d0746746ef5b
psharma2003 <155620291+psharma2003@users.noreply.github.com>
2024-02-19 21:22:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,11 @@ let  navbar=document.querySelector('.navbar');
 menuIcon.onclick=()=>{
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-
-
 }
 
-
-
-
-
-
 //scroll sec
-
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header nav a');
-
-
-
  
 window.onscroll=()=>{
     sections.forEach(sec =>{
@@ -37,22 +26,36 @@ window.onscroll=()=>{
     navLinks.forEach(links=>{
         links.classList.remove('active');
         document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-
-       // document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-    })
+  })
 }
-
-    });
+ });
         //sticky header
     let header=document.querySelector('header');
 
     header.classList.toggle('sticky',window.scrollY>100);
 
-    //remove toggle and navbar wjen click navbar links
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
 }
+
+
+const textSpan = document.querySelectorAll('.text-animate h3 span');
+let index = 0;
+
+function typingEffect() {
+    if (index < textSpan.length) {
+        textSpan[index].classList.add('typing');
+        index = (index + 1) % textSpan.length; // Reset index when it reaches the end
+        setTimeout(typingEffect, 200); // Adjust typing speed (milliseconds)
+    }
+}
+
+typingEffect();
+
+
+
+
 
 
 
